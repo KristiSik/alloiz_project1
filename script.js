e55d20fa8e79439a2acaf36b33205fe92eb1b1e4
KristiSik <krissik98@yandex.ua>
2018-09-21 19:25:52 +0300

    Init

---
+++ script.js
@@ -0,0 +1,31 @@
+var nodeArray = [
+    document.querySelector('.main-text'),
+    document.querySelector('.header-image'),
+    document.querySelector('.arrow-from-header-image'),
+    document.querySelector('.video-section'),
+    document.querySelector('.step:nth-of-type(1)'),
+    document.querySelector('.step:nth-of-type(2)'),
+    document.querySelector('.step:nth-of-type(3)'),
+    document.querySelector('.step:nth-of-type(4)'),
+    document.querySelector('.how-it-works-section .google-play-buttons'),
+    document.querySelector('.about-items-row:nth-child(1)'),
+    document.querySelector('.about-items-row:nth-child(2)')
+];
+ScrollReveal().reveal(nodeArray, { reset: true, interval: 400 });
+
+$(document).ready(function(){
+    var questions = $(".faq-question");
+    $(".faq-answer").each(function() {$(this).hide();});
+    questions.each(function(index) {
+        $(this).click(function() {
+            console.log('asd');
+            if ($("#answer" + (index + 1)).is(":hidden")) {
+                $("#answer" + (index + 1)).slideDown();
+                $(this).parent().addClass("faq-item-expanded")
+            } else {
+                $("#answer" + (index + 1)).slideUp();
+                $(this).parent().removeClass("faq-item-expanded")
+            }
+        });
+    });
+});
